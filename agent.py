b31d0303f56fe3f0977c317da20a944539e87252
Lalanga <jayasinghe.lalanga88@gmail.com>
2026-08-17 13:55:01 +0530

    Implement ModelBasedAgent with memory, visited-cell tracking, and loop-avoidance behavior.

--- agent.py
+++ agent.py
@@ -18,3 +18,59 @@ class SimpleReflexAgent:
             return 'Left'
 
         return 'Right'
+
+class ModelBasedAgent:
+    def __init__(self):
+        self.last_percept = None
+        self.last_action = None
+        self.visited_cells = set()
+        self.relative_pos = (0, 0)
+        self.action_cycle = ['Left', 'Right', 'Up', 'Down']
+        self.cycle_index = 0
+
+    def sense_and_act(self, percept: dict) -> str:
+        if self.last_action is not None and not percept['wall_ahead']:
+            self._update_relative_pos(self.last_action)
+
+        self.visited_cells.add(self.relative_pos)
+
+        repeated_situation = (self.last_percept == percept)
+
+        if percept['food_here']:
+            action = 'Right'
+        elif percept['wall_ahead']:
+            if repeated_situation:
+                self.cycle_index = (self.cycle_index + 1) % len(self.action_cycle)
+            action = self.action_cycle[self.cycle_index]
+        else:
+            candidate = self._forward_cell(self.action_cycle[self.cycle_index])
+            if candidate in self.visited_cells:
+                self.cycle_index = (self.cycle_index + 1) % len(self.action_cycle)
+            action = self.action_cycle[self.cycle_index]
+
+        self.last_percept = dict(percept)
+        self.last_action = action
+        return action
+
+    def _update_relative_pos(self, action):
+        x, y = self.relative_pos
+        if action == 'Up':
+            self.relative_pos = (x, y + 1)
+        elif action == 'Down':
+            self.relative_pos = (x, y - 1)
+        elif action == 'Left':
+            self.relative_pos = (x - 1, y)
+        elif action == 'Right':
+            self.relative_pos = (x + 1, y)
+
+    def _forward_cell(self, action):
+        x, y = self.relative_pos
+        if action == 'Up':
+            return (x, y + 1)
+        elif action == 'Down':
+            return (x, y - 1)
+        elif action == 'Left':
+            return (x - 1, y)
+        elif action == 'Right':
+            return (x + 1, y)
+        return self.relative_pos        
